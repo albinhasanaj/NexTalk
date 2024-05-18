c00dbfc5ff7b133af2cfa1bab57ad95a9077fb2c
PoorGenius <albinhasanaj06@hotmail.com>
2024-05-18 20:20:16 +0200

    Push

--- components/AuthProviderButton.tsx
+++ components/AuthProviderButton.tsx
@@ -2,7 +2,8 @@ import Image from 'next/image'
 
 const AuthProviderButton = ({ provider }: { provider: "google" | "github" }) => {
     return (
-        <button className="flex justify-center items-center py-2 px-4 gap-2 bg-[#D9D9D9] bg-opacity-5 border-[1px] border-white rounded-[5px]">
+        <button className="flex justify-center items-center py-2 px-4 gap-2 bg-[#D9D9D9] bg-opacity-5 border-[1px] border-white rounded-[5px]"
+        >
             <Image src={`/icons/${provider}.svg`}
                 alt={`${provider} icon`}
                 width={32}
--- db/nex_talk_dev.session.sql
+++ db/nex_talk_dev.session.sql
@@ -3,8 +3,10 @@ CREATE TABLE users (
     user_id VARCHAR(36) PRIMARY KEY,
     username VARCHAR(255) UNIQUE NOT NULL,
     email VARCHAR(255) UNIQUE NOT NULL,
-    password VARCHAR(255) NOT NULL
+    password VARCHAR(255) NULL
 );
 -- @block
 SELECT *
 FROM users;
+--@block
+DROP TABLE users;
