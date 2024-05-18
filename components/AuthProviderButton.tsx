be0179129ee9374194ca96b9d170f857236d7097
PoorGenius <albinhasanaj06@hotmail.com>
2024-05-18 20:28:22 +0200

    fixed bug - always submitting form, provider button

--- components/AuthProviderButton.tsx
+++ components/AuthProviderButton.tsx
@@ -3,6 +3,8 @@ import Image from 'next/image'
 const AuthProviderButton = ({ provider }: { provider: "google" | "github" }) => {
     return (
         <button className="flex justify-center items-center py-2 px-4 gap-2 bg-[#D9D9D9] bg-opacity-5 border-[1px] border-white rounded-[5px]"
+            onClick={() => { console.log("implement signup here") }}
+            type="button"
         >
             <Image src={`/icons/${provider}.svg`}
                 alt={`${provider} icon`}
