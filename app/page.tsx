eb6567cfa5edf50228e9ddbdfbde37704711f51a
Oliversoder06 <lexrudolf06@gmail.com>
2024-06-03 05:34:03 +0200

    Added styling for a part of homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,48 @@
+import TryForFreeBtn from "@/components/TryForFreeBtn";
+import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <h1>HEJSAN</h1>
+    <div className="relative m-0 min-h-screen bg-gradient-to-b from-black to-[rgba(255,255,255,0.05)] overflow-hidden">
+      <div className="absolute inset-0">
+        <Image
+          src="/images/grid.png"
+          alt="background"
+          layout="fill"
+          objectFit="cover"
+          className="opacity-25"
+        />
+      </div>
+      <nav className="relative flex justify-between px-[9rem] pt-[2.5rem]">
+        <h1 className="text-white text-[2rem] font-[700]">NexTalk</h1>
+        <div className="flex gap-[1.5rem] text-white">
+          <Link href="/" className="text-[1.5rem] hover:underline"> Home </Link>
+          <Link href="/about" className="text-[1.5rem] hover:underline"> About </Link>
+          <Link href="/login" className="text-[1.5rem] hover:underline"> Log In </Link>
+          <TryForFreeBtn />
+        </div>
+      </nav>
+
+      <div className="relative flex justify-between pl-[10rem] pr-[25rem]">
+        <div>
+          <Image
+            src="/images/chatpreview.png"
+            alt="chat preview"
+            width={640}
+            height={480}
+            className="transition duration-300 ease-in-out hover:scale-[1.02] mt-[8rem]"
+          />
+        </div>
+
+        <div className="flex flex-col justify-center gap-5">
+          <div className="flex flex-col gap-2">
+            <h1 className="text-[3rem] text-white font-[700] leading-[1.1]">REAL TIME <br /> COMMUNICATION</h1>
+            <p className="text-[1.5rem] text-[#ffffff]">From anywhere, with anyone.</p>
+          </div>
+          <TryForFreeBtn />
+        </div>
+      </div>
+    </div>
   );
 }
---
+++ components/TryForFreeBtn.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const TryForFreeBtn = () => {
+    return (
+        <button className="rounded-[5px] before:ease relative h-[2.5rem] w-[9rem] overflow-hidden bg-[#4D4D4D] text-white shadow-2xl transition-all before:absolute before:right-0 before:top-0 before:h-12 before:w-6 before:translate-x-12 before:rotate-6 before:bg-white before:opacity-10 before:duration-700 hover:before:-translate-x-40 hover:bg-[#585858]">
+            Try for free
+        </button>
+    )
+}
+
+export default TryForFreeBtn
