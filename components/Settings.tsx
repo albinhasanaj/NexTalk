9d626d28c5591f0a839057e8e62478b67c96d6df
PoorGenius <albinhasanaj06@hotmail.com>
2024-06-06 21:01:13 +0200

    feat(update-profile): implement feature update-profile

--- components/Settings.tsx
+++ components/Settings.tsx
@@ -1,6 +1,6 @@
 "use client";
 import Image from 'next/image'
-import { ChangeEvent, useRef, useState } from 'react';
+import { ChangeEvent, FormEvent, useRef, useState } from 'react';
 import { useChatSessionStore } from '@/store/useStore';
 import toast from 'react-hot-toast';
 
@@ -19,6 +19,14 @@ const Settings = () => {
         password: '',
         confirmPassword: ''
     });
+
+    const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+        const { name, value } = e.target;
+        setValues(prevState => ({
+            ...prevState,
+            [name]: value
+        }));
+    }
 
     const fileInputRef = useRef(null);
 
@@ -57,6 +65,33 @@ const Settings = () => {
         }
     };
 
+    const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        try {
+            // check if confirmPassword is included
+
+            const response = await fetch('/api/profile/update-profile-credentials', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(values)
+            });
+
+            const data = await response.json();
+
+            if (!response.ok) {
+                throw new Error(data.message || 'Something went wrong');
+            }
+
+            toast.success('Profile updated successfully');
+        } catch (error) {
+            toast.error(error.message);
+            console.error(error);
+        }
+
+    };
+
     return (
         <div className='flex flex-col items-center gap-4 h-full justify-center'>
             <div className="relative w-[100px] h-[100px] mb-6">
@@ -90,24 +125,38 @@ const Settings = () => {
 
             <div className='flex flex-col'>
                 <label htmlFor="ChangeUsername">Change Username</label>
-                <input id='ChangeUsername' type="text" placeholder='johndoe' className="py-2 px-4 rounded-[5px] bg-[#424242] bg-opacity-10 border-[1px] border-white/50 placeholder:text-[14px] text-[14px] outline-none w-[300px]" />
+                <input id='ChangeUsername' type="text" placeholder='johndoe' className="py-2 px-4 rounded-[5px] bg-[#424242] bg-opacity-10 border-[1px] border-white/50 placeholder:text-[14px] text-[14px] outline-none w-[300px]"
+                    name='username'
+                    onChange={handleChange}
+                />
             </div>
 
-            <div className='flex flex-col'>
-                <label htmlFor="ChangeEmailAddress">Change Email Address</label>
-                <input id='ChangeEmailAddress' type="email" placeholder='johndoe@gmail.com' className="py-2 px-4 rounded-[5px] bg-[#424242] bg-opacity-10 border-[1px] border-white/50 placeholder:text-[14px] text-[14px] outline-none w-[300px]" />
-            </div>
+            <form onSubmit={handleSubmit} className="flex flex-col items-center gap-6">
+                <div className='flex flex-col'>
+                    <label htmlFor="ChangeEmailAddress">Change Email Address</label>
+                    <input id='ChangeEmailAddress' type="email" placeholder='johndoe@gmail.com' className="py-2 px-4 rounded-[5px] bg-[#424242] bg-opacity-10 border-[1px] border-white/50 placeholder:text-[14px] text-[14px] outline-none w-[300px]"
+                        name='email'
+                        onChange={handleChange}
+                    />
+                </div>
 
-            <div className='flex flex-col'>
-                <label htmlFor="ChangePassword">Change Password</label>
-                <input id='ChangePassword' type="password" placeholder='******' className="py-2 px-4 rounded-[5px] bg-[#424242] bg-opacity-10 border-[1px] border-white/50 placeholder:text-[14px] text-[14px] outline-none w-[300px]" />
-            </div>
+                <div className='flex flex-col'>
+                    <label htmlFor="ChangePassword">Change Password</label>
+                    <input id='ChangePassword' type="password" placeholder='******' className="py-2 px-4 rounded-[5px] bg-[#424242] bg-opacity-10 border-[1px] border-white/50 placeholder:text-[14px] text-[14px] outline-none w-[300px]"
+                        name='password'
+                        onChange={handleChange}
+                    />
+                </div>
 
-            <div className='flex flex-col'>
-                <label htmlFor="ConfirmPassword">Confirm Password*</label>
-                <input id='ConfirmPassword' type="password" placeholder='******' className="py-2 px-4 rounded-[5px] bg-[#424242] bg-opacity-10 border-[1px] border-white/50 placeholder:text-[14px] text-[14px] outline-none w-[300px]" />
-            </div>
-            <button className='bg-[#4D4D4D] w-[20rem] h-[3rem] rounded-[1rem] outline-none placeholder-[rgba(255,255,255,0.5)] text-white text-[0.75rem] font-normal tracking-[0.0375rem] pl-[1rem] mt-[1rem] ring-1 ring-transparent hover:ring-white/50 hover:bg-[#585858] transition-all duration-200 ease-in-out'>Submit Changes</button>
+                <div className='flex flex-col'>
+                    <label htmlFor="ConfirmPassword">Confirm Password*</label>
+                    <input id='ConfirmPassword' type="password" placeholder='******' className="py-2 px-4 rounded-[5px] bg-[#424242] bg-opacity-10 border-[1px] border-white/50 placeholder:text-[14px] text-[14px] outline-none w-[300px]"
+                        name='confirmPassword'
+                        onChange={handleChange}
+                    />
+                </div>
+                <button className='bg-[#4D4D4D] w-[20rem] h-[3rem] rounded-[1rem] outline-none placeholder-[rgba(255,255,255,0.5)] text-white text-[0.75rem] font-normal tracking-[0.0375rem] pl-[1rem] mt-[1rem] ring-1 ring-transparent hover:ring-white/50 hover:bg-[#585858] transition-all duration-200 ease-in-out'>Submit Changes</button>
+            </form>
         </div>
     )
 }
---
+++ pages/api/profile/update-profile-credentials.ts
@@ -0,0 +1,109 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import bcrypt from "bcrypt";
+import { getUserId } from "@/lib/getUserId";
+
+interface UpdateProfileCredentials {
+    username?: string;
+    email?: string;
+    password?: string;
+    confirmPassword?: string;
+}
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+    if (req.method !== "POST") {
+        return res.status(405).json({ message: "Method not allowed" });
+    }
+
+    const { username, email, password, confirmPassword } = req.body;
+    const userId = await getUserId(req, res);
+    if (!userId) {
+        return res.status(401).json({ message: "Unauthorized" });
+    }
+
+    // check if user is a github user or not
+    const user = await prisma.user.findUnique({
+        where: {
+            id: userId
+        }
+    });
+
+    if (!user) {
+        return res.status(404).json({ message: "User not found" });
+    }
+
+    if (!user.githubId && !confirmPassword) {
+        return res.status(400).json({ message: "Confirm password is required" });
+    }
+
+    const fieldsToUpdate: UpdateProfileCredentials = {};
+
+    if (username.trim() !== "") {
+        const userNameExists = await prisma.user.findFirst({
+            where: {
+                username: username
+            }
+        });
+
+        if (userNameExists) {
+            return res.status(400).json({ message: "Username already exists" });
+        }
+
+        fieldsToUpdate.username = username;
+    }
+
+    if (email.trim() !== "") {
+        const emailExists = await prisma.user.findFirst({
+            where: {
+                email: email
+            }
+        });
+
+        if (emailExists) {
+            return res.status(400).json({ message: "Email already exists" });
+        }
+
+        fieldsToUpdate.email = email;
+    }
+
+    let passwordMatch = null;
+    if (!user.githubId) {
+        passwordMatch = bcrypt.compare(password, user.password!);
+        if (!passwordMatch) {
+            return res.status(400).json({ message: "Invalid password" });
+        }
+    } else {
+        passwordMatch = true;
+    }
+
+    if (!passwordMatch) {
+        return res.status(400).json({ message: "Invalid password" });
+    }
+
+    // hash the new password if it is provided
+    let hashedPassword = null;
+    if (password) {
+        hashedPassword = await bcrypt.hash(password, 10);
+        fieldsToUpdate.password = hashedPassword;
+    }
+
+    // only update the fields that are provided else leave it like it is
+    if (Object.keys(fieldsToUpdate).length > 0) {
+        const updatedUser = await prisma.user.update({
+            where: {
+                id: userId
+            },
+            data: {
+                ...fieldsToUpdate
+            }
+        });
+
+        if (!updatedUser) {
+            return res.status(500).json({ message: "Failed to update user" });
+        }
+
+        return res.status(200).json({ message: "User updated successfully" });
+    } else {
+        res.status(200).json({ message: "User updated successfully" });
+    }
+
+};
