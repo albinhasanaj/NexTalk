edf521362c555538b6ca3c2a846a69b80b05817d
PoorGenius <albinhasanaj06@hotmail.com>
2024-06-07 13:01:58 +0200

    Implement styling logout - responsiveness needed

--- components/Logout.tsx
+++ components/Logout.tsx
@@ -19,10 +19,18 @@ const Logout = () => {
     }
 
     return (
-        <div>
-            <h1>Are you sure you want to logout?</h1>
-            <button onClick={handleLogout}>Yes</button>
-            <button>No</button>
+        <div className="backdrop-blur-3xl w-full h-full flex items-center justify-center">
+            <div className="bg-[#26252A] py-8 px-16 rounded-3xl space-y-4">
+                <p className="text-center font-bold text-[20px]">Are you sure you want <br /> to log out?</p>
+                <div className="flex gap-12 w-full">
+                    <button className="py-2 w-[120px] font-bold bg-[#2AB906] rounded-md"
+                        onClick={() => window.location.href = "/chatpage"}
+                    >Stay</button>
+                    <button className="py-2 w-[120px] font-bold bg-[#D83C3E] rounded-md"
+                        onClick={handleLogout}
+                    >Log Out</button>
+                </div>
+            </div>
         </div>
     )
 }
