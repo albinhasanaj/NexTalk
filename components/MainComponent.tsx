971e75979cbe5da16f368c2ac9c3f626313102e8
PoorGenius <albinhasanaj06@hotmail.com>
2024-06-24 15:32:11 +0200

    Prepare for Vecel Deployment

--- components/MainComponent.tsx
+++ components/MainComponent.tsx
@@ -30,7 +30,7 @@ const MainComponent = ({
     return (
         <div className={`${classNames} w-[300px] sm:w-[350px] h-[500px] sm:h-[600px] md:w-[400px] md:flex rounded-3xl md:rounded-r-3xl md:rounded-tl-none md:rounded-bl-none md:rounded-tr-3xl md:w[500px] lg:w-[600px md:h-[600px] lg:h-[750px] lg:w-[600px] flex-shrink-0 rounded-r-3xl rounded-tr-3xl border border-white/54 bg-white/7 backdrop-blur-[30px] items-center bg-[rgba(255,255,255,0.1)] flex-col text-white`}>
             {/* RIGHT SIDE */}
-            {view !== "NoChatSelected" && view !== "Settings" && view !== "Logout" ? <ChatHeader socket={socket} isConnected={isConnected} /> : null}
+            {view !== "NoChatSelected" && view !== "Settings" && view !== "Logout" ? <ChatHeader /> : null}
 
             {/* TYPES OF VIEWS */}
             {toggleView(view)}
